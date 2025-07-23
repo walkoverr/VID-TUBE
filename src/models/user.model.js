403bde3b56145245da9576afe6d8ec06336caaab
abhishek yadav <abhisathayadav143@gmail.com>
2025-07-23 17:58:54 +0530

    added pre and post hook in the iuserSchema to store the encrypted pass

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -44,4 +44,14 @@ const userSchema = new Schema({
     }
 },{timestamps: true, versionKey: false});
 
+userSchema.pre("save",async function(next){
+    if(!this.modified("password")) return next();
+    this.password= bcrypt.hash(this.password,10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password); 
+}
+
 export const  User  = mongoose.model("User",userSchema)
